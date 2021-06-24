51fff43fa1762be22645a68c7497a9585382634b
Rodrigo Marquez <rod.m89@gmail.com>
2021-06-23 22:13:13 -0400

    added value limits on animal age, mood and health

--- index.py
+++ index.py
@@ -8,26 +8,32 @@ class Animal:
     self.mood = mood
   
   def display_info(self):
+    
+    moodval = ''
+    healthval= ''
 
     if self.mood < 30:
-      self.mood = 'Angry'
+      moodval = 'Angry'
     
-    elif self.mood > 80:
-      self.mood = 'Happy'
+    elif self.mood <50:
+      moodval = 'Stressed'
+    
+    elif self.mood > 75:
+      moodval = 'Happy'
     
     else:
-      self.mood = 'Relaxed'
+      moodval = 'Relaxed'
     
-    if self.health < 70:
-      self.health = 'Unhealthy'
+    if self.health < 75:
+      healthval = 'Unhealthy'
     
     elif self.health < 40:
-      self.health = 'Sick'
+      healthval = 'Sick'
     
     else:
-      self.health = 'Healthy'
+      healthval = 'Healthy'
 
-    print(f'{self.name}, {self.age} years old. Looks {self.health} and {self.mood}')
+    print(f'{self.name}:    \t {self.age} years old. Looks {healthval} and {moodval}')
   
   def feed(self):
 
@@ -38,7 +44,7 @@ class Animal:
 
 
 class Ape(Animal):
-  def __init__(self, name, age=3, health=90, mood=50, hasTail=True):
+  def __init__(self, name, age=3, health=90, mood=80, hasTail=True):
     super().__init__(name, age, health, mood)
   
   def feed(self):
@@ -67,9 +73,23 @@ class Bird(Animal):
   def __repr__(self):
     return self.name
 
+class Reptile(Animal):
+  def __init__(self, name, age=1, health=80, mood=50, hasLegs=True):
+    super().__init__(name, age, health, mood)
+
+  def feed(self):
+
+    self.health += 20
+    self.mood += 5
+
+    print(f'{zoo1.animals[which_one - 1]} was given a fly')
+  
+  def __repr__(self):
+    return self.name
+
 
 class Feline(Animal):
-  def __init__(self, name, age=2, health=70, mood=50, furPattern='Plain'):
+  def __init__(self, name, age=2, health=70, mood=40, furPattern='Plain'):
     super().__init__(name, age, health, mood)
 
   def feed(self):
@@ -100,6 +120,10 @@ class Zoo:
     self.animals.append(Bird(name))
     return self
 
+  def add_reptile(self,name):
+    self.animals.append(Reptile(name))
+    return self
+
   def print_all_info(self):
     print('-'*30, self.name, '-'*30)
     for animal in self.animals:
@@ -113,9 +137,7 @@ class Zoo:
 
 zoo1 = Zoo('Buin Zoo')
 
-zoo1.add_ape('Gorilla').add_ape('Chimpancee')
-zoo1.add_feline('Tiger').add_feline('Lion')
-zoo1.add_bird('Owl').add_bird('Penguin')
+zoo1.add_ape('Gorilla').add_feline('Tiger').add_bird('Penguin').add_reptile('Iguana')
 
 def main():
 
@@ -132,25 +154,105 @@ def main():
     
     if response == '1':
 
-      specie = input('\nWich animal would you like to add? \n1: Ape \n2: Feline \n3: Bird \n0: Exit \n')
+      specie = input('\nWich animal would you like to add? \n1: Ape \n2: Feline \n3: Bird \n4: Reptile \n0: Exit \n')
 
       if specie == '1':
 
         apeName = input('\nApe name: ')
+        apeAge = int(input ('\nAge: '))
+
+        if apeAge<0 or apeAge>50:
+          print('Value must be between 1 and 100')
+          continue
+
+        apeHealth = int(input ('\nHealth (1~100): '))
+        if apeHealth<0 or apeHealth>101:
+          print('Value must be between 1 and 100')
+          continue
+
+        apeMood = int(input ('\nMood level (1~100): '))
+        if apeMood<0 or apeMood>101:
+          print('Value must be between 1 and 100')
+          continue
+
         zoo1.add_ape(apeName)
+        zoo1.animals[len(zoo1.animals)-1].age = apeAge
+        zoo1.animals[len(zoo1.animals)-1].health = apeHealth
+        zoo1.animals[len(zoo1.animals)-1].mood = apeMood
+
         print(f'\nCongratulations, {apeName} is now part of our zoo!\n')
 
       elif specie == '2':
 
-        felineName = input('\nFeline name: ')
+        felineName = input('\nfeline name: ')
+        felineAge = int(input ('\nAge: '))
+
+        if felineAge<0 or felineAge>50:
+          print('Value must be between 1 and 100')
+          continue
+
+        felineHealth = int(input ('\nHealth (1~100): '))
+        if felineHealth<0 or felineHealth>101:
+          print('Value must be between 1 and 100')
+          continue
+
+        felineMood = int(input ('\nMood level (1~100): '))
+        if felineMood<0 or felineMood>101:
+          print('Value must be between 1 and 100')
+          continue
+
         zoo1.add_feline(felineName)
-        print(f'\nCongratulations, {felineName} is now part of our zoo!\n')
+        zoo1.animals[len(zoo1.animals)-1].age = felineAge
+        zoo1.animals[len(zoo1.animals)-1].health = felineHealth
+        zoo1.animals[len(zoo1.animals)-1].mood = felineMood
 
       elif specie == '3':
 
-        birdName = input('\nBird name: ')
+        birdName = input('\nbird name: ')
+        birdAge = int(input ('\nAge: '))
+
+        if birdAge<0 or birdAge>50:
+          print('Value must be between 1 and 100')
+          continue
+
+        birdHealth = int(input ('\nHealth (1~100): '))
+        if birdHealth<0 or birdHealth>101:
+          print('Value must be between 1 and 100')
+          continue
+
+        birdMood = int(input ('\nMood level (1~100): '))
+        if birdMood<0 or birdMood>101:
+          print('Value must be between 1 and 100')
+          continue
+
         zoo1.add_bird(birdName)
-        print(f'\nCongratulations, {birdName} is now part of our zoo!\n')
+        zoo1.animals[len(zoo1.animals)-1].age = birdAge
+        zoo1.animals[len(zoo1.animals)-1].health = birdHealth
+        zoo1.animals[len(zoo1.animals)-1].mood = birdMood
+
+      elif specie == '4':
+
+        reptileName = input('\nreptile name: ')
+        reptileAge = int(input ('\nAge: '))
+
+        if reptileAge<0 or reptileAge>50:
+          print('Value must be between 1 and 100')
+          continue
+
+        reptileHealth = int(input ('\nHealth (1~100): '))
+        if reptileHealth<0 or reptileHealth>101:
+          print('Value must be between 1 and 100')
+          continue
+
+        reptileMood = int(input ('\nMood level (1~100): '))
+        if reptileMood<0 or reptileMood>101:
+          print('Value must be between 1 and 100')
+          continue
+
+        zoo1.add_reptile(reptileName)
+        zoo1.animals[len(zoo1.animals)-1].age = reptileAge
+        zoo1.animals[len(zoo1.animals)-1].health = reptileHealth
+        zoo1.animals[len(zoo1.animals)-1].mood = reptileMood
 
       elif specie =='0':
 
